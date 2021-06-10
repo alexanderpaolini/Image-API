c7a45d1939fef61168ad2d97c30fa12474a3c42f
Million900o <apaolini900o@gmail.com>
2021-06-09 22:00:06 -0400

    adding rotate image

---
+++ src/images/rotate.ts
@@ -0,0 +1,99 @@
+// @ts-expect-error
+import GIFEncoder from 'gif-encoder-2'
+
+import Canvas from 'canvas'
+
+import { Meme } from '.'
+
+const PARTS = 22
+
+export default {
+  name: 'rotate',
+  cacheResponse: true,
+  contentType: 'image/gif',
+  parser: (req, res) => req.query,
+  validator: (api, d) => d.url && typeof d.url === 'string' && api.utils.validateUrl(d.url),
+  exec: async (api, { url, angles }, { req, res }) => {
+    const width = 512
+    const height = 512
+
+    const image = await Canvas.loadImage(url)
+    const canvas = Canvas.createCanvas(width, height)
+
+    const ctx = canvas.getContext('2d')
+
+    const encoder = new GIFEncoder(width, width)
+    encoder.setTransparent(0x402814)
+
+    encoder.start()
+    encoder.setRepeat(0)
+    encoder.setDelay(30)
+
+    for (let i = 0; i <= PARTS; i++) {
+      ctx.save()
+
+      // Reset the data
+      api.utils.clearCanvasCtx(ctx, width, height)
+
+      // Draw the image
+      const wr = 10 * i
+      const fw = width - (wr * 2)
+
+      ctx.drawImage(image, wr, 0, fw, height)
+      encoder.addFrame(ctx)
+
+      ctx.restore()
+    }
+
+    for (let i = PARTS; i >= 0; i--) {
+      ctx.save()
+
+      // Reset the data
+      api.utils.clearCanvasCtx(ctx, width, height)
+
+      const wr = 10 * i
+      const fw = width - (wr * 2)
+      api.utils.drawImageWithRotation(ctx, image, wr, 0, fw, height, undefined, true)
+
+      encoder.addFrame(ctx)
+
+      ctx.restore()
+    }
+
+    for (let i = 0; i <= PARTS; i++) {
+      ctx.save()
+
+      // Reset the data
+      api.utils.clearCanvasCtx(ctx, width, height)
+
+      const wr = 10 * i
+      const fw = width - (wr * 2)
+
+      api.utils.drawImageWithRotation(ctx, image, wr, 0, fw, height, undefined, true)
+
+      encoder.addFrame(ctx)
+
+      ctx.restore()
+    }
+
+    for (let i = PARTS; i >= 0; i--) {
+      ctx.save()
+
+      // Reset the data
+      api.utils.clearCanvasCtx(ctx, width, height)
+
+      // Draw the image
+      const wr = 10 * i
+      const fw = width - (wr * 2)
+
+      ctx.drawImage(image, wr, 0, fw, height)
+      encoder.addFrame(ctx)
+
+      ctx.restore()
+    }
+
+    encoder.finish()
+
+    return encoder.out.getData()
+  }
+} as Meme<{ url: string, angles: string }>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -139,4 +139,63 @@ export class Utils {
   validateUrl (url: string): boolean {
     return this.urlRegex.test(url)
   }
+
+  /**
+   * Clear the canvas
+   * @param ctx Canvas CTX
+   * @param width How long
+   * @param height How high
+   */
+  clearCanvasCtx (ctx: Canvas.CanvasRenderingContext2D, width: number, height: number): void {
+    const imageData = ctx.getImageData(0, 0, width, height)
+
+    for (let j = 0; j < imageData.data.length; j += 4) {
+      imageData.data[j] = 64
+      imageData.data[j + 1] = 40
+      imageData.data[j + 2] = 20
+      imageData.data[j + 3] = 0
+    }
+
+    ctx.putImageData(imageData, 0, 0)
+  }
+
+  drawImageWithRotation (
+    ctx: Canvas.CanvasRenderingContext2D,
+    img: Canvas.Image,
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    deg?: number,
+    flip?: any,
+    flop?: any
+  ): void {
+    ctx.save()
+
+    if (typeof width === 'undefined') width = img.width
+    if (typeof height === 'undefined') height = img.height
+
+    // Set the origin to the center of the image
+    ctx.translate(x + width / 2, y + height / 2)
+
+    // Rotate the canvas around the origin
+    if (deg) {
+      const rad = 2 * Math.PI - deg * Math.PI / 180
+      ctx.rotate(rad)
+    }
+
+    // Flip the canvas
+    let flipScale
+    if (flip) flipScale = -1; else flipScale = 1
+
+    // Flop the canvas
+    let flopScale
+    if (flop) flopScale = -1; else flopScale = 1
+    ctx.scale(flipScale, flopScale)
+
+    // Draw the image
+    ctx.drawImage(img, -1 * width / 2, -1 * height / 2, width, height)
+
+    ctx.restore()
+  }
 }
